8ef4d59c5e79c65e1b9279a61fe512f9ebca7e2d
AliAsjad512 <aa0504503@gmail.com>
2025-09-16 19:02:35 -0400

    Javascipt for kanban

--- kanboard/index.js
+++ kanboard/index.js
@@ -1,13 +1,18 @@
-
-const UserInput= document.getElementById('userInput');
-
-const CreateTask= document.getElementById('create');
-const todoBoard= document.getElementById('todo')
-
-CreateTask.addEventListener('click',()=> {
-const CreateDiv= document.createElement('div');
-CreateDiv.append(UserInput.value);
-todoBoard.append(CreateDiv);
+const AllBoard = document.querySelectorAll('.board');
 
 
-})
+
+// Get the element by id
+const Allelement = document.querySelectorAll(".item");
+
+for(let i=0;i<Allelement.length;i++){
+    //Allelement[i].classList.add('draggable')
+    
+}
+
+
+
+// element.addEventListener("dragstart", (ev) => {
+  
+  
+// });
