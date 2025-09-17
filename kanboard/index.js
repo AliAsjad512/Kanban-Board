d942215bcaac606547f15a877bcfce562d7ac2ff
AliAsjad512 <aa0504503@gmail.com>
2025-09-17 19:11:02 -0400

    start dragging

--- kanboard/index.js
+++ kanboard/index.js
@@ -1,54 +1,50 @@
 const AllBoard = document.querySelectorAll('.board');
 
+const Allpara= document.querySelectorAll('.item');
 
-
-// Get the element by id
-const Allelement = document.querySelectorAll(".item");
-
-for(let i=0;i<Allelement.length;i++){
-    Allelement[i].setAttribute('draggable',true);
-
-    const target= document.querySelectorAll("#target");
-
-
-target.forEach((item)=>{
+Allpara.forEach((p) =>{
+  p.addEventListener("dragstart", (ev) =>{
     
-const paragraphs = item.querySelectorAll("p"); // all <p> inside this div
+  })
 
-  paragraphs.forEach((p) => {
-    console.log(p.textContent); // do something with each <p>
-  });
-
-    
 })
+
     
-}
 
 
+AllBoard.forEach((item)=>{
+ //onsole.log(item)
 
-// for(let i=0;i<targ.length;i++){
-//     console.log(targ[i].value);
-// }
-
-
-
-
-
-// element.addEventListener("dragstart", (ev) => {
   
-  
-// });
-
-
-//     item.addEventListener("dragover", (ev) => {
-//   ev.preventDefault();
-//   ev.dataTransfer.dropEffect = "move";
-// });
-// item.addEventListener("drop", (ev) => {
-//   ev.preventDefault();
-//   // Get the id of the target and add the moved element to the target's DOM
-//   const data = ev.dataTransfer.getData(Allelement[i].textContent)
-//   console.log(data)
-//   ev.item.appendChild(data)
  
-// });
+});
+
+
+    
+
+
+
+  
+
+
+
+   
+  
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
