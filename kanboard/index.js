6f708edd10f628f0eaca7c379dbc9e7dad9fbe20
AliAsjad512 <aa0504503@gmail.com>
2025-09-18 18:59:29 -0400

    done

--- kanboard/index.js
+++ kanboard/index.js
@@ -4,16 +4,39 @@ const Allpara= document.querySelectorAll('.item');
 
 Allpara.forEach((p) =>{
   p.addEventListener("dragstart", (ev) =>{
+    console.log("dragStart");
+    ev.currentTarget.classList.add("dragging")
+    ev.dataTransfer.clearData();
+    ev.dataTransfer.setData("text",ev.target.id);
     
   })
+ 
 
 })
+
 
     
 
 
 AllBoard.forEach((item)=>{
- //onsole.log(item)
+ 
+  item.addEventListener("dragover",(ev) =>{
+    ev.preventDefault();
+    console.log("DragOver");
+
+  })
+
+  item.addEventListener("drop",(ev) =>{
+    ev.preventDefault();
+    // console.log("item:", item);
+    console.log("ev.target:", ev.target);
+    //console.log("ev.currentTarget:", ev.currentTarget);
+
+   const data = ev.dataTransfer.getData("text");
+    // console.log(data)
+    
+     ev.currentTarget.appendChild(document.getElementById(data))
+  })
 
   
  
