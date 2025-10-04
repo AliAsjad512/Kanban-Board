10daeff93fc662c31ff67816deec7703488ffa04
AliAsjad512 <aa0504503@gmail.com>
2025-10-03 21:54:13 -0400

    latest

--- kanboard/index2.js
+++ kanboard/index2.js
@@ -3,7 +3,7 @@
 const addTaskBtn = document.getElementById('add-task-btn');
 const todoBoard = document.getElementById('todo-board');
 const allBoards = document.querySelectorAll('.board');
-const AddBoardBtn=document.getElementById('add-board');
+
 const AddNewBoard = document.getElementById('add-board');
 const mainCon = document.querySelector('.container')
 
@@ -15,17 +15,18 @@ AddNewBoard.addEventListener('click',()=>{
     const headi=prompt('Enter board name');
     const boardName =document.createElement('h4');
 boardName.innerText=headi;
+newBoard.appendChild(boardName)
 
-board.addEventListener('dragover', () => {
+newBoard.addEventListener('dragover', () => {
          
         const flyingElement = document.querySelector('.flying');
         
-            if(board.firstElementChild){
-               board.insertBefore(flyingElement, board.firstElementChild)
+            if(newBoard.firstElementChild){
+               newBoard.insertBefore(flyingElement, newBoard.firstElementChild)
 
             }
             else{
-             board.appendChild(flyingElement);
+             newBoard.appendChild(flyingElement);
             }
                 
            
@@ -39,13 +40,73 @@ board.addEventListener('dragover', () => {
     AddBtn.classList.add('Task');
 
     AddBtn.innerText = 'Add Task';
-    newBoard.appendChild(AddBtn)
-    
-     newBoard.appendChild(boardName);
-    mainCon.appendChild(newBoard);
+AddBtn.addEventListener('click',() =>{
+const input=prompt('Enter your task');
+if(!input)
+    return
+
+const taskCard = document.createElement('div');
+const taskText = document.createElement('p');
+taskText.classList.add('item')
+taskText.innerText = input;
+taskCard.setAttribute('draggable',true);
+
+taskCard.addEventListener('dragstart',() =>{
+    taskCard.classList.add('flying')
+})
+taskCard.addEventListener('dragend',() =>{
+    taskCard.classList.remove('flying')
+})
+
+ 
+
+ const EditBtn=document.createElement('button')
+
+    //EditBtn.classList.add('Task');
+
+    EditBtn.innerText = 'Edit Task';
+
+
+EditBtn.addEventListener('click',()=>{
+   const newText = prompt('Enter the new value', taskText.innerText);
+    if (newText) taskText.innerText = newText;
+
+})
+
+
+const DeleteBtn = document.createElement('button');
+DeleteBtn.innerText = 'Delete';
+DeleteBtn.addEventListener('click',() =>{
+    taskCard.remove();
+})
+
+
+
+taskCard.appendChild(taskText);
+taskCard.appendChild(EditBtn);
+taskCard.appendChild(DeleteBtn);
+
+newBoard.appendChild(taskCard);
+
+
+
+})
+
+
+
+AddItem.appendChild(AddBtn);
+
+newBoard.appendChild(AddItem);
+
+
+mainCon.appendChild(newBoard);
+
+
    
 
 })
+
+
 
 
 
